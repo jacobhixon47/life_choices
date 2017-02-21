be1b58786334f8b1fe422ab3346d1008ecccdd99
Jacob Hixon and Skye Atwood <jacob@>
2017-02-21 11:02:26 -0800

    implement AJAX for adding new questions

--- life_choices/app/controllers/questions_controller.rb
+++ life_choices/app/controllers/questions_controller.rb
@@ -3,6 +3,34 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    @question.chose_one = 0
+    @question.chose_two = 0
+    if @question.save
+      respond_to do |format|
+        format.html { redirect_to questions_path }
+        format.js
+      end
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+  end
+
+private
+  def question_params
+    params.require(:question).permit(:option_one, :option_two)
+
   end
 end
